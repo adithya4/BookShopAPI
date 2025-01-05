224caeb14b2cbd1ec966537eaf4957a4196b80c9
adithya4 <adithyas255@gmail.com>
2025-01-05 21:58:23 +0530

    added carts

--- views/models.py
+++ views/models.py
@@ -1,8 +1,11 @@
 from django.db import models
-
+from uuid import uuid4
 # Create your models here.
 class Collection(models.Model):
     collection_type = models.CharField(max_length=50)
+
+    class Meta:
+        ordering = ['id']
 
     def __str__(self):
         return self.collection_type
@@ -13,5 +16,22 @@ class Books(models.Model):
     description = models.TextField()
     collection = models.ForeignKey(Collection, on_delete= models.CASCADE, related_name="books")
 
+    class Meta:
+        ordering = ['id']
+
     def __str__(self):
         return self.title
+    
+class Carts(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid4)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+class CartItem(models.Model):
+    cart = models.ForeignKey(Carts, on_delete=models.CASCADE, related_name='items')
+    book = models.ForeignKey(Books,on_delete=models.CASCADE)
+
+    class Meta:
+        unique_together = [['cart','book']]
+
+    def __str__(self):
+        return str(self.cart)
--- views/serializers.py
+++ views/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import Books, Collection
+from .models import Books, Collection, Carts, CartItem
 class BooksSerializer(serializers.ModelSerializer):
     class Meta:
         model = Books
@@ -11,3 +11,30 @@ class CollectionSerializer(serializers.Serializer):
 
     def books_list(self, collection:Collection):
         return collection.books.all()
+
+class CartItemSerializer(serializers.ModelSerializer):
+    # Include the book details directly using the related `Books` serializer
+    book_details = serializers.SerializerMethodField(method_name='get_book_details')
+
+    def get_book_details(self, cart_item):
+        # Access the book details through the foreign key relationship
+        book = cart_item.book
+        return {
+            "id": book.id,
+            "title": book.title,
+            "author": book.author,
+            "description": book.description,
+            "collection": book.collection.collection_type  # Assuming you want the collection type
+        }
+
+    class Meta:
+        model = CartItem
+        fields = ['id', 'book_details']  # Add 'book_details' to the serialized fields
+
+
+class CartSerializer(serializers.ModelSerializer):
+    id = serializers.UUIDField(read_only=True)
+    items = CartItemSerializer(many=True)
+    class Meta:
+        model = Carts
+        fields=['id', 'items'] 
